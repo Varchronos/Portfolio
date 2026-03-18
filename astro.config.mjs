36399909e8132a7b10eca9a630ca0f2d5c217c21
Varchronos <babu.dhruvrajak@gmail.com>
2026-03-18 17:01:51 +0530

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()]
+	},
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), icon()],
 });
