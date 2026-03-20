2e7515f6c4a8aff20bd0b8a8a81d422275daa296
Varchronos <babu.dhruvrajak@gmail.com>
2026-03-20 21:06:20 +0530

    update sharing stats and thumbnail

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Dhruv Rajak';
-export const SITE_DESCRIPTION = 'Welcome to my portfolio!';
+export const SITE_DESCRIPTION = 'Fullstack Web & React Native Developer ';
